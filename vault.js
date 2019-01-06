b9cd91a794495cfba97f24fe200c228a077af5dc
Samantha Juntilla <juntilla@hawaii.edu>
2019-01-05 15:19:44 -1000

    Completes assignment.

--- vault.js
+++ vault.js
@@ -1,4 +1,21 @@
 'use strict';
-module.exports = function() {
-  
+module.exports = function () {
+    let obj = {};
+
+    function getValue(key, value) {
+        if (obj[key] === undefined) {
+            return null;
+        } else {
+            return obj[key];
+        }
+    }
+
+    function setValue(key, value) {
+        obj[key] = value;
+    }
+
+    return {
+        getValue,
+        setValue
+    }
 };
